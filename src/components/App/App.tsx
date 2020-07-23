2ecc977593508040da2e7c10109f2d534ad131e3
Aditya Donthy <Aditya.Donthy@thetrainline.com>
2020-07-23 23:14:29 +0530

    Added the html and css for Calendar component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Recorder from '../Recorder/Recorder';
+import Calendar from './../Calendar/Calendar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Recorder />
+        <Calendar />
       </header>
     </div>
   );
---
+++ src/components/Calendar/Calendar.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './calendar.css';
+
+const Calendar: React.FC = () => {
+    return(
+        <div className="calendar-container">
+            <div className="calendar-day">
+                <div className="calendar-day-label">
+                    <span></span>
+                </div>
+                <div className="calendar-events">
+                    <div className="calendar-event">
+                        <div className="calendar-event-info">
+                            <div className="calendar-event-time">10:00 - 12:00</div>
+                            <div className="calendar-event-title">Learning TS</div>
+                        </div>
+                        <button className="calendar-event-delete-button">&times;</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Calendar;
